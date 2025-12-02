a5340bf4c4445ed19c6c10983a629330c34c01b4
thabang077 <64046663+thabang077@users.noreply.github.com>
2025-12-02 07:11:28 +0200

    Configure CORS for specific origins
    Added CORS configuration to allow requests from specified origins.

--- ReactWebApiBloodType/Program.cs
+++ ReactWebApiBloodType/Program.cs
@@ -11,9 +11,16 @@ builder.Services.AddControllers();
 
 builder.Services.AddDbContext<DonationDBContext>(options =>
 
-{ 
-options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
-});
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+
+}
+
+
+);
+
+builder.Services.AddCors();
+
 
 
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
@@ -22,13 +29,22 @@ builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
+app.UseCors(options => options.WithOrigins("" +
+    "http://localhost:3000",
+    "http://bloodtypeapi.runasp.net"
+    )
+.AllowAnyHeader()
+.AllowAnyMethod());
+
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+  
 
+app.UseHttpsRedirection();
 app.UseAuthorization();
 
 app.MapControllers();
